bd1d47999fc09e2ba84c41085aa9b718b732186a
Khaled Hamed <khaled.hamedt@gmail.com>
2017-10-19 19:22:42 +0200

    2016-2017 ACM-ICPC Pacific Northwest Regional Contest (Div. 1)

---
+++ 101201 - 2016-2017 ACM-ICPC Pacific Northwest Regional Contest (Div. 1)/C.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+const int di[] = { -1, 0, 1, 0 };
+const int dj[] = { 0, 1, 0, -1 };
+const ll MOD = 1e9 + 7;
+const ll INF = 1e10;
+const double EPS = 1e-10;
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0);
+	int n, k, r; cin >> n >> k >> r;
+	vb h(n);
+	for (int i = 0; i < k; i++) {
+		int x; cin >> x;
+		h[x] = true;
+	}
+
+	int cur = 0;
+	for (int i = 0; i < r; i++)
+		cur += h[i];
+
+	int rs = 0;
+	if (cur < 2) {
+		if (!h[r - 1]) h[r - 1] = true;
+		else h[r - 2] = true;
+		cur++, rs++;
+	}
+	if (cur < 2) {
+		if (!h[r - 1]) h[r - 1] = true;
+		else h[r - 2] = true;
+		cur++, rs++;
+	}
+
+	for (int i = r; i < n; i++) {
+		cur += h[i];
+		cur -= h[i - r];
+		if (cur < 2) {
+			if (!h[i]) h[i] = true;
+			else h[i - 1] = true;
+			cur++, rs++;
+		}
+	}
+	cout << rs << endl;
+	cin.ignore(), cin.get();
+}
---
+++ 101201 - 2016-2017 ACM-ICPC Pacific Northwest Regional Contest (Div. 1)/I.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+const int di[] = { -1, 0, 1, 0 };
+const int dj[] = { 0, 1, 0, -1 };
+const ll MOD = 1e9 + 7;
+const ll INF = 1e10;
+const double EPS = 1e-10;
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+
+int n, k;
+deque<pair<ll, ll>> h;
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0);
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		int x, m; cin >> x >> m;
+		if (x)
+			h.push_back(mp(x, m));
+	}
+	h.push_back(mp(0, 0));
+	sort(all(h));
+
+	ll r = 0;
+	while (h.front().first) {
+		ll x = h.front().first, m = h.front().second;
+		h.pop_front();
+		r += -x * 2 * (m / k);
+		m %= k;
+
+		if (m) {
+			ll ck = k - m;
+			r += -x * 2;
+			while (h.front().first && ck) {
+				if (ck >= h.front().second) {
+					ck -= h.front().second;
+					h.pop_front();
+				}
+				else {
+					h.front().second -= ck;
+					ck = 0;
+				}
+			}
+		}
+	}
+	while (h.back().first) {
+		ll x = h.back().first, m = h.back().second;
+		h.pop_back();
+		r += x * 2 * (m / k);
+		m %= k;
+
+		if (m) {
+			ll ck = k - m;
+			r += x * 2;
+			while (h.back().first && ck) {
+				if (ck >= h.back().second) {
+					ck -= h.back().second;
+					h.pop_back();
+				}
+				else {
+					h.back().second-=ck;
+					ck = 0;
+				}
+			}
+		}
+	}
+	cout << r << endl;
+	cin.ignore(), cin.get();
+}
