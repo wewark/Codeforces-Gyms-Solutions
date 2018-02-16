03cdc016ac2cc171c3825719df9b2c16f479006e
Khaled Hamed <khaled.hamedt@gmail.com>
2018-02-16 15:26:25 +0200

    2017 ACM-ICPC World Finals

---
+++ 101471 - 2017 ACM-ICPC World Finals/E.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define fs first
+#define sc second
+#define pb push_back
+
+using namespace std;
+
+typedef long long ll;
+const int N=101, mod=60;
+const ll INF=1e18+7;
+const double PI = 3.141592653589793238462643383279502884197, EPS=1e-9, two=2.0;
+
+int n, T;
+vector<double> d, s;
+
+double f(double c) {
+	double ret = 0;
+	for (int i = 0; i < n; ++i) {
+		if (s[i] + c <= EPS) return 1e9;
+		ret += d[i] / (s[i] + c);
+	}
+	return ret;
+}
+
+double bs() {
+	double a = -1e9, b = 1e9, mid = (a + b) / two;
+	while (b - a > EPS) {
+		if (f(mid) > T)
+			a = mid;
+		else
+			b = mid;
+		mid = (a + b) / two;
+	}
+
+	return mid;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	cin >> n >> T;
+	d.resize(n); s = d;
+	for (int i = 0; i < n; ++i) {
+		cin >> d[i] >> s[i];
+	}
+
+	cout << fixed << setprecision(9) << bs() << endl;
+
+	return 0;
+}
---
+++ 101471 - 2017 ACM-ICPC World Finals/I.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define fs first
+#define sc second
+#define pb push_back
+
+using namespace std;
+
+typedef long long ll;
+const int N=300, mod=60;
+const double PI = 3.141592653589793238462643383279502884197, eps=1e-6;
+
+vector<int> g[N];
+bool vis[N];
+
+bool check(int x, int y)
+{
+	if(x==y)
+		return 1;
+
+	bool ans=0;
+
+	vis[x]=1;
+
+	for(unsigned int i=0 ; i<g[x].size() && !ans; i++)
+		if(!vis[g[x][i]])
+			ans |= check(g[x][i],y);
+
+	return ans;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+	int n, m;
+	char ch1, ch2;
+	string s1, s2;
+	bool f;
+
+	cin >> m >> n;
+
+	while(m--)
+	{
+		cin >> ch1 >> ch2;
+		g[ch1].pb(ch2);
+	}
+
+	while(n--)
+	{
+		cin >> s1 >> s2;
+
+		f=0;
+
+		if(s1.size()==s2.size())
+		{
+			f=1;
+
+			for(unsigned int i=0 ; i<s1.size() && f ; i++)
+			{
+				f=check(s1[i],s2[i]);
+				memset(vis,0,sizeof vis);
+			}
+		}
+
+		cout << (f ? "yes" : "no") << '\n';
+	}
+
+	return 0;
+}
