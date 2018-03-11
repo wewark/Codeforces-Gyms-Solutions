e68f2793a4d0cc9cca881a5b95b17ee664bd4afe
Khaled Hamed <khaled.hamedt@gmail.com>
2018-03-11 20:43:06 +0200

    100187L

---
+++ 100187 - 2013, VI Samara Regional Intercollegiate Programming Contest/L.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<short, int> pii;
+typedef vector<pii> vii;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+const int di[] = { -1,0,1,0 };
+const int dj[] = { 0,1,0,-1 };
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0);
+	string s; cin >> s;
+	int a = 0, b = s.length() - 1;
+	while (a < b) {
+		s[b--] = s[a++];
+	}
+	cout << s << endl;
+	cin.ignore(), cin.get();
+}
