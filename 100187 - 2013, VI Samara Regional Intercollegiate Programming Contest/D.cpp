fce6a618e827095a2ab1a1868ee79471fcea698b
Khaled Hamed <khaled.hamedt@gmail.com>
2018-03-11 20:42:11 +0200

    100187D Combinatorics, Mod Inverse

---
+++ 100187 - 2013, VI Samara Regional Intercollegiate Programming Contest/D.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+const int di[] = { -1,0,1,0 };
+const int dj[] = { 0,1,0,-1 };
+const ll MOD = 1e9 + 9;
+const ll INF = 1e9;
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+
+vll mem3(2 * 1e5 + 1, -1);
+ll bigmod(ll a, int p, int m) {
+	if (mem3[a] != -1) return mem3[a];
+	if (p == 0)
+		return 1;
+	if (p % 2 == 1)
+		return mem3[a] = ((a % m) * (bigmod(a, p - 1, m) % m)) % m;
+	ll c = bigmod(a, p / 2, m);
+	return mem3[a] = (c * c) % m;
+}
+
+vll mem1(2 * 1e5 + 1, -1);
+ll fact(ll n) {
+	if (mem1[n] != -1) return mem1[n];
+	return mem1[n] = (n == 0 ? 1 : (n * fact(n - 1)) % MOD);
+}
+
+vll mem2(2 * 1e5 + 1, -1);
+ll factInv(ll n) {
+	if (mem2[n] != -1) return mem2[n];
+	if (n == 0)
+		return 1;
+	return mem2[n] = (bigmod(n, MOD - 2, MOD) * factInv(n - 1)) % MOD;
+}
+
+ll nCr(ll n, ll r) {
+	return (((fact(n) * factInv(r)) % MOD) * factInv(n - r)) % MOD;
+}
+
+ll bigmod2(ll a, int p, int m) {
+	if (p == 0)
+		return 1;
+	if (p % 2 == 1)
+		return ((a % m) * (bigmod2(a, p - 1, m) % m)) % m;
+	ll c = bigmod2(a, p / 2, m);
+	return (c * c) % m;
+}
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0);
+	for (int i = 1; i < 200001; i += 1000)
+		fact(i), factInv(i);
+	int n; cin >> n;
+	ll r = 0;
+	for (int i = 1; i <= n; i++) {
+		r += nCr(n, i) * (bigmod2(2, n - i, MOD) - 1);
+		r %= MOD;
+	}
+	cout << r << endl;
+	cin.ignore(), cin.get();
+}
