1ece98a3325f0e8b51c85e29d3d14cc46d7ce135
Khaled Hamed <khaled.hamedt@gmail.com>
2018-03-29 21:23:35 +0200

    101239A

---
+++ 101239 - 2015 ACM-ICPC World Finals/A.cpp
@@ -0,0 +1,22 @@
+#include < bits/stdc++.h >
+using namespace std;
+
+int main() {
+	int p, a, b, c, d, n;
+	double price, mx = 0, mn;
+
+	cin >> p >> a >> b >> c >> d >> n;
+
+	for (int i = n; i; i--) {
+		price = p * (sin(a * i + b) + cos(c * i + d) + 2);
+
+		if (i == n)
+			mn = price;
+		else {
+			mx = max(mx, price - mn);
+			mn = min(mn, price);
+		}
+	}
+
+	cout << fixed << setprecision(11) << mx;
+}
