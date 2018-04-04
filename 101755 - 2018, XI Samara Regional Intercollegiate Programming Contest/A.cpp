d43883261b20c3c7a8b1da221aac46eb3d913201
Khaled Hamed <khaled.hamedt@gmail.com>
2018-04-04 11:55:48 +0200

    101755A

---
+++ 101755 - 2018, XI Samara Regional Intercollegiate Programming Contest/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pii;
+typedef vector<pii> vii;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+const int di[] = { -1, 0, 1, 0, -1, 1, 1, -1 };
+const int dj[] = { 0, 1, 0, -1, -1, 1, -1, 1 };
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const long double EPS = 1e-10;
+#define all(n) n.begin(),n.end()
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0);
+	int s, g; cin >> s >> g;
+	if (s % g == 0 && s != g) {
+		cout << s - g << " " << g << endl;
+	}
+	else
+		cout << -1 << endl;
+	cin.ignore(), cin.get();
+}
