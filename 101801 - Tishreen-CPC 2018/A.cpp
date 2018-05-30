b7b3df9a36cab6d82b8ae1a3c870a448fb41e8f9
Khaled Hamed <khaled.hamedt@gmail.com>
2018-05-30 23:58:50 +0200

    101801A

---
+++ 101801 - Tishreen-CPC 2018/A.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+const int di[] = { 0, 0, 1, -1 };
+const int dj[] = { 1, -1, 0, 0 };
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const long double EPS = 1e-6;
+#define all(x) x.begin(),x.end()
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0);
+	int t; cin >> t;
+	while (t--) {
+		int s, d; cin >> s >> d;
+		cout << (s >= d ? "YES" : "NO") << endl;
+	}
+	cin.ignore(), cin.get();
+}
