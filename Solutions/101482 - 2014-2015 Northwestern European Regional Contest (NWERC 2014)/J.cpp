96864cebb8d94074390c0f9ff5b12672340a7909
Khaled Hamed <khaled.hamedt@gmail.com>
2018-09-02 10:28:21 +0200

    101482J

---
+++ Solutions/101482 - 2014-2015 Northwestern European Regional Contest (NWERC 2014)/J.cpp
@@ -0,0 +1,57 @@
+// 2014-2015 Northwestern European Regional Contest (NWERC 2014)
+// 101482J
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop(i,n) for(int i = 0;i < int(n);i++)
+#define rloop(i,n) for(int i = int(n);i >= 0;i--)
+#define range(i,a,b) for(int i = int(a);i <= int(b);i++)
+#define SZ(c) int(c.size())
+#define ALL(c) c.begin(), c.end()
+#define RALL(c) c.rbegin(), c.rend()
+#define PI acos(-1)
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+
+typedef vector<int> vi;
+typedef vector<pair<int, int> > vii;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+map<string, int> M;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	//freopen("in.in", "r", stdin);
+	//freopen("out.in", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+	int n;
+	cin >> n;
+	string s;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		M[s]++;
+	}
+	int res = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		if (M[s])
+			res++, M[s]--;
+	}
+	cout << res << endl;
+	
+	
+	return 0;
+}
