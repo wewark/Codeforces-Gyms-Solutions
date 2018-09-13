df850f9c7dc193738d3f7e9cdd8cf23bf75e8f97
Khaled Hamed <khaled.hamedt@gmail.com>
2018-09-13 11:56:49 +0200

    101608D

---
+++ Solutions/101608 - 2017 ACM Jordanian Collegiate Programming Contest/D.cpp
@@ -0,0 +1,88 @@
+// 2017 ACM Jordanian Collegiate Programming Contest
+// 101608D
+
+#include <bits/stdc++.h>
+#include <ext/hash_map>
+#include <ext/numeric>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+#define REP(i,n) for( (i)=0 ; (i)<(n) ; (i)++ )
+#define rep(i,x,n) for( (i)=(x) ; (i)<(n) ; (i)++ )
+#define REV(i,n) for( (i)=(n) ; (i)>=0 ; (i)-- )
+#define FORIT(it,x) for( (it)=(x).begin() ; (it)!=(x).end() ; (it)++ )
+#define foreach(it,c) for(__typeof((c).begin()) it=(c).begin();it!=(c).end();++it)
+#define rforeach(it,c) for(__typeof((c).rbegin()) it=(c).rbegin();it!=(c).rend();++it)
+#define foreach2d(i, j, v) foreach(i,v) foreach(j,*i)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define SZ(x) ((int)(x).size())
+#define MMS(x,n) memset(x,n,sizeof(x))
+#define mms(x,n,s) memset(x,n,sizeof(x)*s)
+#define pb push_back
+#define mp make_pair
+#define NX next_permutation
+#define UN(x) sort(all(x)),x.erase(unique(all(x)),x.end())
+#define CV(x,n) count(all(x),(n))
+#define FIND(x,n) find(all(x),(n))-(x).begin()
+#define ACC(x) accumulate(all(x),0)
+#define PPC(x) __builtin_popcountll(x)
+#define LZ(x) __builtin_clz(x)
+#define TZ(x) __builtin_ctz(x)
+#define mxe(x) *max_element(all(x))
+#define mne(x) *min_element(all(x))
+#define low(x,i) lower_bound(all(x),i)
+#define upp(x,i) upper_bound(all(x),i)
+#define NXPOW2(x) (1ll << ((int)log2(x)+1))
+#define PR(x) cout << #x << " = " << (x) << endl ;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef pair<int, int> pii;
+
+const int OO = (int) 2e9;
+const double eps = 1e-9;
+const int N = 1e6 + 4;
+int freq[N] , vis[N] , visid;
+int a[5005];
+int n;
+
+void f(int x){
+    if(vis[x] != visid){
+        vis[x] = visid;
+        freq[x] = 0;
+    }
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+//	freopen("in.txt", "rt", stdin);
+//	freopen("out.txt", "wt", stdout);
+#endif
+	freopen("cases.in", "rt", stdin);
+	int t;
+	scanf("%d",&t);
+	while (t--) {
+		scanf("%d",&n);
+		for (int i = 0; i < n; i++)
+			scanf("%d",&a[i]);
+
+		int res = 0;
+		for (int i = 0; i < n; i++) {
+			visid++;
+			int odd = 0;
+			for (int j = i; j < n; j++) {
+                f(a[j]);
+                if(!(freq[a[j]]&1))odd++;
+                else odd--;
+                freq[a[j]]++;
+                res += (odd == 1);
+			}
+		}
+		printf("%d\n",res);
+	}
+	return 0;
+}
