b529260ce00e206c36c56492c56d14cea20b39c2
Khaled Hamed <khaled.hamedt@gmail.com>
2018-11-25 13:44:08 +0200

    101972D

---
+++ Solutions/101972 - ACM International Collegiate Programming Contest Arabella Collegiate Programming Contest (2018)/D.cpp
@@ -0,0 +1,42 @@
+// ACM International Collegiate Programming Contest Arabella Collegiate Programming Contest (2018)
+// 101972D
+
+#include <bits/stdc++.h>
+#include <ext/hash_set>
+#include <ext/numeric>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+const ll MOD = 1e9 + 7;
+const long double EPS = 1e-5;
+#define all(x) x.begin(),x.end()
+#define SZ(x) (int)x.size()
+#define fr first
+#define sc second
+
+
+int main() {
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+#endif
+
+	int t; cin >> t;
+	while (t--) {
+		int n, x, y; cin >> n >> x >> y;
+		if (x >= (n + 1) / 2 && y >= n / 2)
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+	}
+
+
+}
