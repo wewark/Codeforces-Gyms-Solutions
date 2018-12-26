3c1680ada70dd548abfd7a57bd62a91cb64f94f9
Khaled Hamed <khaled.hamedt@gmail.com>
2018-12-26 21:20:01 +0200

    100198B Bipartite matching, better solution

--- Solutions/100198 - 2003-2004 Summer Petrozavodsk Camp Andrew Stankevich Contest 3 (ASC 3)/B.cpp
+++ Solutions/100198 - 2003-2004 Summer Petrozavodsk Camp Andrew Stankevich Contest 3 (ASC 3)/B.cpp
@@ -21,57 +21,31 @@ int di[] = { 0, 0, 1, -1 };
 int dj[] = { 1, -1, 0, 0 };
 #define all(x) x.begin(),x.end()
 
-struct BipartiteMatcher {
-	vector<vector<int>> G;
-	vector<int> L, R, Viz;
-
-	BipartiteMatcher(int n, int m) :
-			G(n), L(n, -1), R(m, -1), Viz(n) {}
-
-	void AddEdge(int a, int b) {
-		G[a].push_back(b);
-	}
-
-	bool Match(int node) {
-		if (Viz[node])
-			return false;
-		Viz[node] = true;
-
-		for (auto vec : G[node]) {
-			if (R[vec] == -1) {
-				L[node] = vec;
-				R[vec] = node;
+bool FindMatch(int i, const vvi &w, vi &mr, vi &mc, vi &seen) {
+	for (int j = 0; j < w[i].size(); j++) {
+		if (w[i][j] && !seen[j]) {
+			seen[j] = true;
+			if (mc[j] < 0 || FindMatch(mc[j], w, mr, mc, seen)) {
+				mr[i] = j;
+				mc[j] = i;
 				return true;
 			}
 		}
-
-		for (auto vec : G[node]) {
-			if (Match(R[vec])) {
-				L[node] = vec;
-				R[vec] = node;
-				return true;
-			}
-		}
-
-		return false;
 	}
+	return false;
+}
 
-	int Solve() {
-		int ok = true;
-//		while (ok--) {
-			for (int i = 0; i < (int)L.size(); ++i) {
-				fill(Viz.begin(), Viz.end(), 0);
-				if (L[i] == -1)
-					ok |= Match(i);
-			}
-//		}
+int BipartiteMatching(const vvi &w, vi &mr, vi &mc) {
+	mr = vi(w.size(), -1);
+	mc = vi(w[0].size(), -1);
 
-		int ret = 0;
-		for (int i = 0; i < L.size(); ++i)
-			ret += (L[i] != -1);
-		return ret;
+	int ct = 0;
+	for (int i = 0; i < w.size(); i++) {
+		vi seen(w[0].size());
+		if (FindMatch(i, w, mr, mc, seen)) ct++;
 	}
-};
+	return ct;
+}
 
 vi A;
 
@@ -89,7 +63,7 @@ int main() {
 		scanf("%d", &A[i]);
 	}
 
-	BipartiteMatcher bm(1000, 1000);
+	vvi w(400, vi(400));
 	vector<pair<int, vi>> prf(n);
 	for (int i = 0; i < n; ++i) {
 		int sz; scanf("%d", &sz);
@@ -103,14 +77,17 @@ int main() {
 		return A[a.first] > A[b.first];
 	});
 	for (int i = 0; i < n; ++i) {
-		for (int& j : prf[i].second)
-			bm.AddEdge(i, j);
+		for (int& j : prf[i].second) {
+			w[i][j] = true;
+		}
 	}
 
-	bm.Solve();
+	vi mr, mc;
+	BipartiteMatching(w, mr, mc);
 	vi ans(n);
-	for (int i = 0; i < n; ++i)
-		ans[prf[i].first] = bm.L[i];
+	for (int i = 0; i < n; ++i) {
+		ans[prf[i].first] = mr[i];
+	}
 	for (int i = 0; i < n; ++i) {
 		printf("%d%c", ans[i] + 1, " \n"[i == n - 1]);
 	}
