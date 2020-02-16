276fb6ffa6ce8a0e54390a18416230da3fd2bd9d
wewark <khaled.hamedt@gmail.com>
2020-02-16 20:48:40 +0200

    102428E

--- Solutions/102428 - 2019-2020 ACM-ICPC Latin American Regional Programming Contest/E.cpp
+++ Solutions/102428 - 2019-2020 ACM-ICPC Latin American Regional Programming Contest/E.cpp
@@ -4,58 +4,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using vi = vector<ll>;
+using vi = vector<int>;
 using vvi = vector<vi>;
 const int INF = 1e9;
-const ll MOD = 1e9 + 7;
-
-const int N = 2e3 + 3;
-int n, l;
-vvi adj;
-
-vi mem(N, -1);
-ll solve(int cur) {
-    if (cur >= l) return 1;
-    ll& ret = mem[cur];
-    if (~ret) return ret;
-
-    ret = 0;
-    for (auto& i : adj[cur]) {
-        ret += solve(i);
-        ret %= MOD;
-    }
-    return ret;
-}
-
-
-vi mem2(N, -1);
-ll solve2(int cur) {
-    ll& ret = mem2[cur];
-    if (~ret) return 0;
-    if (cur >= l) return ret = 1;
-
-    ret = 0;
-    for (auto& i : adj[cur]) {
-        ret += solve2(i);
-        ret %= MOD;
-    }
-    return ret;
-}
-
+ 
+ 
 int main() {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
 #endif
-
-    cin >> n >> l;
-    adj.assign(l, vi());
-    for (int i = 0; i < l; ++i) {
-        int sz; cin >> sz;
-        for (int j = 0; j < sz; ++j) {
-            int v; cin >> v; v--;
-            adj[i].push_back(v);
+ 
+    string c; cin >> c;
+    ll s; cin >> s;
+    c += c;
+ 
+    int a = 0, b = 0, n = c.length() / 2;
+    int ce = 0;
+    ll ans = 0;
+    while (a < n) {
+        while (b - a < s && ce == 0) {
+            ce += (c[b] == 'E');
+            b++;
         }
+ 
+        if (ce)
+            ans += s - (b - a) + 1;
+        ce -= (c[a] == 'E');
+        a++;
     }
-
-    cout << solve(0) << " " << solve2(0) << endl;
+ 
+    cout << ans << endl;
 }
