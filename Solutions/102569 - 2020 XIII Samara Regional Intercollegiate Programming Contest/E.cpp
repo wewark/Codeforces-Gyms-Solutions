2d077a2e7eb161c29286672df166c9bad8eed41f
wewark <khaled.hamedt@gmail.com>
2020-10-24 12:55:34 +0200

    102569E

---
+++ Solutions/102569 - 2020 XIII Samara Regional Intercollegiate Programming Contest/E.cpp
@@ -0,0 +1,47 @@
+// 2020 XIII Samara Regional Intercollegiate Programming Contest
+// 102569E
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using vb = vector<bool>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vs = vector<string>;
+#define all(x) x.begin(), x.end()
+// int di[] = {0, 1, 0, -1};
+// int dj[] = {1, 0, -1, 0};
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const double PI = 3.14159265359;
+
+
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+#else
+	//	freopen("document.in", "r", stdin);
+//	freopen("document.out", "w", stdout);
+#endif
+
+	int n; cin >> n;
+	vi v(n);
+	ll sum = 0;
+	for (int i = 0; i < n; ++i) {
+		cin >> v[i];
+		if (i)
+			v[i] += v[i - 1];
+	}
+
+	ll mn = *min_element(all(v));
+	ll ans = 0;
+	if (mn < 0)
+		ans = abs(mn);
+	cout << ans << endl;
+}
